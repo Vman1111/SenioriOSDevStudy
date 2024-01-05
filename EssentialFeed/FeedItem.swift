ddf9375621efff2fc7713610c413c665cd89c44f
Vman1111 <visap@protonmail.com>
2024-01-05 15:47:48 +0000

    Create the FeedItem model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Vytautas Sapranavicius on 05/01/2024.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
