248ce8fadcd66687cf796ab76c096480a22a7147
Vman1111 <visap@protonmail.com>
2024-01-21 17:28:11 +0000

    Move `HTTPClient` to its own file

---
+++ EssentialFeed/Feed API/HTTPClient.swift
@@ -0,0 +1,14 @@
+//
+//  Created by Vytautas Sapranavicius on 21/01/2024.
+//
+
+import Foundation
+
+public enum HTTPClientResult {
+    case success(Data, HTTPURLResponse)
+    case failure(Error)
+}
+
+public protocol HTTPClient {
+    func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void)
+}
