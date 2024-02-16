8e8ec1084f373101539d8f87263dfa0c01e2faf7
Vytautas Sapranavicius <vytautas.sapranavicius@verizonconnect.com>
2024-02-16 13:47:22 +0000

    Move `FeedStore` to its own file

---
+++ EssentialFeed/Feed Cache/FeedStore.swift
@@ -0,0 +1,13 @@
+//
+//  Created by Vytautas Sapranavicius on 16/02/2024.
+//
+
+import Foundation
+
+public protocol FeedStore {
+    typealias DeletionCompletion = (Error?) -> Void
+    typealias InsertionCompletion = (Error?) -> Void
+
+    func deleteCachedFeed(completion: @escaping DeletionCompletion)
+    func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertionCompletion)
+}
