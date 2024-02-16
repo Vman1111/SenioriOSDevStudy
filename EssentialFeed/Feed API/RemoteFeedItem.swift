4052d8f8bf4d05b149e2f0515ba1e3a1c80e47e0
Vytautas Sapranavicius <vytautas.sapranavicius@verizonconnect.com>
2024-02-16 13:59:50 +0000

    Move `RemoteFeedItem` to its own file

---
+++ EssentialFeed/Feed API/RemoteFeedItem.swift
@@ -0,0 +1,13 @@
+//
+//  Created by Vytautas Sapranavicius on 16/02/2024.
+//
+
+import Foundation
+
+internal struct RemoteFeedItem: Decodable {
+    internal let id: UUID
+    internal let description: String?
+    internal let location: String?
+    internal let image: URL
+}
+
