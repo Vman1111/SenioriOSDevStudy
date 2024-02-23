cfeda6cbd84b5ea495abcb32319ac5d0ce6ad205
Vytautas Sapranavicius <vytautas.sapranavicius@verizonconnect.com>
2024-02-23 18:57:58 +0000

    `CoreDataFeedStore.retrieve()` delivers empty on empty cache

---
+++ EssentialFeed/Feed Cache/CoreDataFeedStore.swift
@@ -0,0 +1,22 @@
+//
+//  Created by Vytautas Sapranavicius on 23/02/2024.
+//
+
+import Foundation
+
+public final class CoreDataFeedStore: FeedStore {
+    public init() {}
+
+    public func retrieve(completion: @escaping RetrievalCompletion) {
+        completion(.empty)
+    }
+
+    public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
+
+    }
+
+    public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
+
+    }
+
+}
