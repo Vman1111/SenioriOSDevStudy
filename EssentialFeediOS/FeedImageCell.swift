71babb56c91abcc02c988fa62b74fcadfd77e5a7
Vytautas Sapranavicius <vytautas.sapranavicius@verizonconnect.com>
2024-02-27 17:32:33 +0000

    Render loaded feed on successful load completion

---
+++ EssentialFeediOS/FeedImageCell.swift
@@ -0,0 +1,11 @@
+//
+//  Created by Vytautas Sapranavicius on 27/02/2024.
+//
+
+import UIKit
+
+public final class FeedImageCell: UITableViewCell {
+    public let locationContainer = UIView()
+    public let locationLabel = UILabel()
+    public let descriptionLabel = UILabel()
+}
