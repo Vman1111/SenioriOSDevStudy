0878c8343dc5e3fc60917b9b4cfbb1ae5d546582
Vytautas Sapranavicius <vytautas.sapranavicius@verizonconnect.com>
2024-02-27 16:44:53 +0000

    Add `EssentialFeediOS`framework (prod and test) targets for iOS platform-specific components

---
+++ EssentialFeediOSTests/EssentialFeediOSTests.swift
@@ -0,0 +1,15 @@
+//
+//  EssentialFeediOSTests.swift
+//  EssentialFeediOSTests
+//
+//  Created by Vytautas Sapranavicius on 27/02/2024.
+//
+
+import XCTest
+@testable import EssentialFeediOS
+
+final class EssentialFeediOSTests: XCTestCase {
+
+    
+
+}
