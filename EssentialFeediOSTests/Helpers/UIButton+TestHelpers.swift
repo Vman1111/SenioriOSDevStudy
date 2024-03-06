e6fdffd5736777eb93b52deeaea6046c8110591f
Vytautas Sapranavicius <vytautas.sapranavicius@verizonconnect.com>
2024-03-06 15:47:39 +0000

    Extract UIButton test helper to a separate file

---
+++ EssentialFeediOSTests/Helpers/UIButton+TestHelpers.swift
@@ -0,0 +1,15 @@
+//
+//  Created by Vytautas Sapranavicius on 06/03/2024.
+//
+
+import UIKit
+
+extension UIButton {
+    func simulateTap() {
+        allTargets.forEach { target in
+            actions(forTarget: target, forControlEvent: .touchUpInside)?.forEach {
+                (target as NSObject).perform(Selector($0))
+            }
+        }
+    }
+}
