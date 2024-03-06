30c4590c28002f99d9264521ac3bac1dd013bbcb
Vytautas Sapranavicius <vytautas.sapranavicius@verizonconnect.com>
2024-03-06 18:02:13 +0000

    Extract `FeedImageCell` test helpers to a separate file

---
+++ EssentialFeediOSTests/Helpers/FeedImageCell+TestHelpers.swift
@@ -0,0 +1,36 @@
+//
+//  Created by Vytautas Sapranavicius on 06/03/2024.
+//
+
+import UIKit
+import EssentialFeediOS
+
+extension FeedImageCell {
+    func simulateRetryAction() {
+        feedImageRetryButton.simulateTap()
+    }
+
+    var isShowingLocation: Bool {
+        return !locationContainer.isHidden
+    }
+
+    var isShowingImageLoadingIndicator: Bool {
+        return feedImageContainer.isShimmering
+    }
+
+    var isShowingRetryAction: Bool {
+        return !feedImageRetryButton.isHidden
+    }
+
+    var locationText: String? {
+        return locationLabel.text
+    }
+
+    var descriptionText: String? {
+        return descriptionLabel.text
+    }
+
+    var renderedImage: Data? {
+        return feedImageView.image?.pngData()
+    }
+}
