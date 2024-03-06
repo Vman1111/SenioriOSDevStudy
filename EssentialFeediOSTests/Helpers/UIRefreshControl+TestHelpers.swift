b2534f7fe7473dba463970d94f8be72ef56ee085
Vytautas Sapranavicius <vytautas.sapranavicius@verizonconnect.com>
2024-03-06 15:45:04 +0000

    Extract UIRefreshControl test helper to a separate file

---
+++ EssentialFeediOSTests/Helpers/UIRefreshControl+TestHelpers.swift
@@ -0,0 +1,15 @@
+//
+//  Created by Vytautas Sapranavicius on 06/03/2024.
+//
+
+import UIKit
+
+extension UIRefreshControl {
+    func simulatePullToRefresh() {
+        allTargets.forEach { target in
+            actions(forTarget: target, forControlEvent: .valueChanged)?.forEach {
+                (target as NSObject).perform(Selector($0))
+            }
+        }
+    }
+}
