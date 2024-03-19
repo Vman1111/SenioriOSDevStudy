15dde4f8522fe104260630aa8f0fb8d136332261
Vytautas Sapranavicius <vytautas.sapranavicius@verizonconnect.com>
2024-03-19 17:47:11 +0000

    Display image with animation

---
+++ EssentialFeediOS/Feed UI/Views/UIImageView+Animations.swift
@@ -0,0 +1,18 @@
+//
+//  Created by Vytautas Sapranavicius on 19/03/2024.
+//
+
+import UIKit
+
+extension UIImageView {
+    func setImageAnimated(_ newImage: UIImage?) {
+        image = newImage
+
+        guard newImage != nil else { return }
+
+        alpha = 0
+        UIView.animate(withDuration: 0.25) {
+            self.alpha = 1
+        }
+    }
+}
