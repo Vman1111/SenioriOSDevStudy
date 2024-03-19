ed88c51bf73472f506af3f6ba74b52066c64683e
Vytautas Sapranavicius <vytautas.sapranavicius@verizonconnect.com>
2024-03-19 13:43:16 +0000

    Move ViewModels to separate files

---
+++ EssentialFeediOS/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,7 @@
+//
+//  Created by Vytautas Sapranavicius on 19/03/2024.
+//
+
+struct FeedLoadingViewModel {
+    let isLoading: Bool
+}
---
+++ EssentialFeediOS/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  Created by Vytautas Sapranavicius on 19/03/2024.
+//
+
+
+import EssentialFeed
+
+struct FeedViewModel {
+    let feed: [FeedImage]
+}
