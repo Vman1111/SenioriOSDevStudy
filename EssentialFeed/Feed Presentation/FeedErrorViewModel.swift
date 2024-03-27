178319a29a323447c13166d274b40fde5e436525
Vytautas Sapranavicius <vytautas.sapranavicius@verizonconnect.com>
2024-03-27 12:41:26 +0000

    Move `FeedErrorViewModel` to separate file

---
+++ EssentialFeed/Feed Presentation/FeedErrorViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  Created by Vytautas Sapranavicius on 27/03/2024.
+//
+
+public struct FeedErrorViewModel {
+    public let message: String?
+
+    static var noError: FeedErrorViewModel {
+        return FeedErrorViewModel(message: nil)
+    }
+
+    static func error(message: String) -> FeedErrorViewModel {
+        return FeedErrorViewModel(message: message)
+    }
+}
