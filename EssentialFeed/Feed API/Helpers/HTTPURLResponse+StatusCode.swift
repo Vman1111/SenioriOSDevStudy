d7ee16a719c52b292fb7261a8832ddc0fa216f63
Vytautas Sapranavicius <vytautas.sapranavicius@verizonconnect.com>
2024-04-02 17:04:20 +0100

    Extract duplicate `HTTPURLResponse.statusCode == 200` validation into a `isOK` helper method

---
+++ EssentialFeed/Feed API/Helpers/HTTPURLResponse+StatusCode.swift
@@ -0,0 +1,13 @@
+//
+//  Created by Vytautas Sapranavicius on 02/04/2024.
+//
+
+import Foundation
+
+extension HTTPURLResponse {
+    private static var OK_200: Int { return 200 }
+
+    var isOK: Bool {
+        return statusCode == HTTPURLResponse.OK_200
+    }
+}
