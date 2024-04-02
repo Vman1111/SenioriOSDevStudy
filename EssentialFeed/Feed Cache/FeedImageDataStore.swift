65708aa4737a8805900202572ae83deea116a78e
Vytautas Sapranavicius <vytautas.sapranavicius@verizonconnect.com>
2024-04-02 17:34:46 +0100

    Move `LocalFeedImageDataLoader` and `FeedImageDataStore` to production

---
+++ EssentialFeed/Feed Cache/FeedImageDataStore.swift
@@ -0,0 +1,11 @@
+//
+//  Created by Vytautas Sapranavicius on 02/04/2024.
+//
+
+import Foundation
+
+public protocol FeedImageDataStore {
+    typealias Result = Swift.Result<Data?, Error>
+
+    func retrieve(dataForURL url: URL, completion: @escaping (Result) -> Void)
+}
